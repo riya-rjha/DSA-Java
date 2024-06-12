9eb65c5a8220798e9b7a4d7cabd07e3369bdfbb6
RRJ <riyaranjanjha2006@gmail.com>
2024-06-12 22:30:16 +0530

    Day 2 Optimization

--- Learn.md
+++ Learn.md
@@ -22,7 +22,14 @@ Sort the array & store it in the expected array. Compare the values & increment 
 
 Create a hashmap which holds every value and the number of times it occurred in the array. Create an arraylist and append all the numbers in it in the same order as that of second array. Create a second arraylist for the remaining numbers of first array and add both the arraylists using Collections method. At last store the values in an array & return.
 
+TC - O(n^2)
+
 ### Optimized Soln 
+
+Create a counting array which stores the number of times each element occurs in the array1. Further, if the element of array2 in the respective order is iterated to add it in the arrayList until the value of countingArray does not become equal to zero. Furthermore, the remaining elements are added in the arrayList in ascending order by iterating upto the largest value of array, such that all elements that come across do so in an ascending order. At last, the arrayList is returned as an array. 
+
+TC - O(n)
 
 ## Sort Colors (Medium)
 
+Given an array, sort it in-place. Used counting sort for better time complexity & ease. 
--- relativeSortArray.java
+++ relativeSortArray.java
@@ -47,8 +47,31 @@ public class relativeSortArray {
         return result;
     }
 
-    public static int[] optSortRelArr(int arr1[], int arr2[]){
-
+    // Using Counting Sort
+    public static int[] optSortRelArr(int arr1[], int arr2[]) {
+        int largest = Arrays.stream(arr1).max().orElse(0); // Finding largest element of array
+        int countArr[] = new int[largest + 1];
+        // Create counting array with each number of array1
+        for (int i : arr1) {
+            countArr[i]++;
+        }
+        // Add elements of array2 in resp order in result arrayList
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i : arr2) {
+            while (countArr[i] != 0) {
+                result.add(i);
+                countArr[i]--;
+            }
+        }
+        // Add all remaining elements of array1 in result arrayList
+        for (int i = 0; i <= largest; i++) {
+            while (countArr[i] > 0) {
+                result.add(i);
+                countArr[i]--;
+            }
+        }
+        // Convert the result arrayList to array & return
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
@@ -58,7 +81,8 @@ public class relativeSortArray {
         int arr1[] = { 28, 6, 22, 8, 44, 17 };
         int arr2[] = { 22, 28, 8, 6 };
 
-        int ans[] = sortRelArr(arr1, arr2);
+        // int ans[] = sortRelArr(arr1, arr2);
+        int ans[] = optSortRelArr(arr1, arr2);
         System.out.println(Arrays.toString(ans));
     }
 }
