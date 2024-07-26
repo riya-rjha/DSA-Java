9b720c827dfa682d2280a70fee145a5a9135cd9f
RRJ <riyaranjanjha2006@gmail.com>
2024-07-26 23:55:43 +0530

    Second Largest Number

---
+++ DSA Striver Sheet/secondLargestNo.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class secondLargestNo {
+
+    // 2 Methods - Brute Force -> Find largest element in array then check if each
+    // element of array is not equal to the largest element and compare with every
+    // successive second largest element
+
+    public int print3largest(List<Integer> arr) {
+        // Code Here
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = -1;
+        for (int i = 0; i < arr.size(); i++) {
+            largest = Math.max(largest, arr.get(i));
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            if (secondLargest < arr.get(i) && arr.get(i) != largest) {
+                secondLargest = arr.get(i);
+            }
+        }
+        return secondLargest;
+    }
+
+    // Optimized - If the current element is largest, then the element which was
+    // largest before it will be the second largest element. Thus, for every largest
+    // element you update also update the second largest element. And the corner
+    // case would be that if any element occurs that is not the largest element but
+    // is still greater than the secondLargest element, then sepcify a condition for
+    // the same and update only second largest element in that case.
+
+    public int print2largest(List<Integer> arr) {
+        int largest = arr.get(0);
+        int sec = -1;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) > largest) {
+                sec = largest;
+                largest = arr.get(i);
+            }
+            if (arr.get(i) < largest && arr.get(i) > sec) {
+                sec = arr.get(i);
+            }
+        }
+        return sec;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> lst = new ArrayList<>();
+        secondLargestNo num1 = new secondLargestNo();
+        secondLargestNo num2 = new secondLargestNo();
+        lst.add(12);
+        lst.add(35);
+        lst.add(1);
+        lst.add(10);
+        lst.add(34);
+        lst.add(1);
+        System.out.println("Optimal Algo: " + num1.print2largest(lst));
+        System.out.println("Brute Force: " + num2.print3largest(lst));
+    }
+}
--- SearchingSorting/linearSearch.java
+++ SearchingSorting/linearSearch.java
@@ -1,5 +1,3 @@
-package SearchingSorting;
-
 import java.util.*;
 
 class linearSearch {
@@ -63,16 +61,17 @@ class linearSearch {
         int arr[] = { 10, -3, 72, 83, 16, 3, 81 };
         System.out.println(findMin(arr));
         System.out.println("Enter elments of 2D Array: ");
-        Scanner sc = new Scanner(System.in);
-        int newArr[][] = new int[3][2];
-        for (int i = 0; i < newArr.length; i++) {
-            System.out.println("Enter elements of row " + (i + 1));
-            for (int j = 0; j < newArr[i].length; j++) {
-                System.out.println("Enter element of column " + (j + 1));
-                newArr[i][j] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int newArr[][] = new int[3][2];
+            for (int i = 0; i < newArr.length; i++) {
+                System.out.println("Enter elements of row " + (i + 1));
+                for (int j = 0; j < newArr[i].length; j++) {
+                    System.out.println("Enter element of column " + (j + 1));
+                    newArr[i][j] = sc.nextInt();
+                }
             }
+            int target2 = 38;
+            System.out.println("Element found at index: " + (Arrays.toString(findInTwoDarray(newArr, target2))));
         }
-        int target2 = 38;
-        System.out.println("Element found at index: " + (Arrays.toString(findInTwoDarray(newArr, target2))));
     }
 }
