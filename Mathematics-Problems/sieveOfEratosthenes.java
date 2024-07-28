939f6c7eed3abb086103334a42ee6c8720cb78fc
RRJ <riyaranjanjha2006@gmail.com>
2024-07-28 15:25:20 +0530

    Sieve of Eratosthenes

--- Mathematics-Problems/sieveOfEratosthenes.java
+++ Mathematics-Problems/sieveOfEratosthenes.java
@@ -1,3 +1,29 @@
+import java.util.Arrays;
+
 public class sieveOfEratosthenes {
-    
+
+    public static void printPrimes(int n) {
+        boolean isPrime[] = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j = j + i) {
+                    isPrime[j] = false; // not prime for every multiple of i
+                }
+            }
+        }
+
+        for (int i = 0; i < isPrime.length; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        printPrimes(n);
+    }
 }
