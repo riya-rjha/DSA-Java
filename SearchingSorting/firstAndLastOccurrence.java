794b0f080847c212f53210726a3727680ed5930d
RRJ <riyaranjanjha2006@gmail.com>
2024-07-30 00:56:23 +0530

    First and Last Occurrence of target element

---
+++ SearchingSorting/firstAndLastOccurrence.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class firstAndLastOccurrence {
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = { -1, -1 };
+        ans[0] = helperFunction(nums, target, true); // find first occurrence
+        ans[1] = helperFunction(nums, target, false); // find last occurrence
+        return ans;
+    }
+
+    public int helperFunction(int nums[], int target, boolean isFirstOccurrence) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                idx = mid;
+                if (isFirstOccurrence) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 5, 7, 7, 7, 7, 8, 8, 10 };
+        // int nums[] = { 1 };
+        int target = 7;
+        firstAndLastOccurrence searchRangeAns = new firstAndLastOccurrence();
+        System.out.println(Arrays.toString(searchRangeAns.searchRange(nums, target)));
+    }
+}
