419577c44ed2803833f8b0dc2a8e4ccfb365f13a
RRJ <riyaranjanjha2006@gmail.com>
2024-08-02 01:33:48 +0530

    Trying CF Qs

---
+++ ContestsQs/SortingCF.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SortingCF {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int t = sc.nextInt();
+            while (t != 0) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                if (x < y) {
+                    System.out.println(x + " " + y);
+                } else {
+                    System.out.println(y + " " + x);
+                }
+                t--;
+            }
+        }
+
+    }
+}
