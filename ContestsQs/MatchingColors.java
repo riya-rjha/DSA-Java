9151e784fea7332707213dbddab1b5078bf7762a
RRJ <riyaranjanjha2006@gmail.com>
2024-08-04 15:40:22 +0530

    Hashmap approach for Matching Colors

--- ContestsQs/MatchingColors.java
+++ ContestsQs/MatchingColors.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class MatchingColors {
 
@@ -19,10 +20,42 @@ public class MatchingColors {
         return count;
     }
 
+    public int winPlayerOptimized(int n, int [] [] pick){
+        HashMap<Integer, HashMap<Integer, Integer>> hm = new HashMap<>();
+
+        // Add for every nth player in hashmap and initialize
+        for (int i = 0; i < pick.length; i++) {
+            hm.put(i, new HashMap<>());
+        }
+
+        for (int[] currPlayer : pick) { 
+            int x = currPlayer[0];
+            int y = currPlayer[1];
+            // for every player x add current color y in the hashmap and for that y
+            // add number of occurrence
+            hm.get(x).put(y, hm.get(x).getOrDefault(y, 0) +1);
+        }
+
+        int count = 0;
+        for (int key : hm.keySet()) {
+            HashMap<Integer, Integer> map = hm.get(key);
+            for (int yMap : map.keySet() ) {
+                if(key < map.get(yMap)){
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+
+    }
+
     public static void main(String[] args) {
         int n = 4;
         int[][] pick = { { 0, 0 }, { 1, 0 }, { 1, 0 }, { 2, 1 }, { 2, 1 }, { 2, 0 } };
         MatchingColors ans = new MatchingColors();
         System.out.println(ans.winningPlayerCount(n, pick));
+        System.out.println(ans.winPlayerOptimized(n, pick));
     }
 }
