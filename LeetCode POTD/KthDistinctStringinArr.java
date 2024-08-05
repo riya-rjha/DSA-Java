b321e64386542293b86d6d072ac8837012b5692d
RRJ <riyaranjanjha2006@gmail.com>
2024-08-05 22:19:30 +0530

    Optimized Approach for Kth Distinct String Array

--- LeetCode POTD/KthDistinctStringinArr.java
+++ LeetCode POTD/KthDistinctStringinArr.java
@@ -1,4 +1,5 @@
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 
 public class KthDistinctStringinArr {
@@ -27,10 +28,45 @@ public class KthDistinctStringinArr {
         return "";
     }
 
+    public String kthDistinctOptimized(String [] arr, int k){
+        LinkedHashSet<String> distinctSet = new LinkedHashSet<>();
+        LinkedHashSet<String> nonDistinctSet = new LinkedHashSet<>();
+
+        int totalDistinctCount = 0;
+
+        for (String str : arr) {
+            if(!nonDistinctSet.contains(str)){
+                if(!distinctSet.contains(str)){
+                    distinctSet.add(str);
+                    totalDistinctCount++;
+                }else{
+                    distinctSet.remove(str);
+                    nonDistinctSet.add(str);
+                    totalDistinctCount--;
+                }
+            }
+        }
+
+        if(totalDistinctCount < k) return "";
+
+        for (String str : distinctSet) {
+            if(distinctSet.contains(str)){
+                k--;
+            }
+            if(k == 0){
+                return str;
+            }
+        }
+
+        return "";
+
+    }
+
     public static void main(String[] args) {
         String arr[] = { "aaa","aa","a" };
         int k = 1;
         KthDistinctStringinArr abs = new KthDistinctStringinArr();
         System.out.println(abs.kthDistinct(arr, k));
+        System.out.println(abs.kthDistinctOptimized(arr, k));
     }
 }
