e74bf6cb684126ff2b5a909864767f6a7bf34a20
riya-rjha <riyaranjanjha2006@gmail.com>
2024-10-11 13:57:45 +0530

    Add ReorganizeArr class for rearranging an array

---
+++ LeetCode POTD/ReorganizeArr.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReorganizeArr {
+
+    public List<Integer> rearrange(List<Integer> arr) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < arr.size(); i++) {
+            ans.add(-1);
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) >= 0 && arr.get(i) < ans.size()) {
+                ans.set(arr.get(i), arr.get(i));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> lst = Arrays.asList(-1, -1, 6, 1, 9, 3, 2, -1, 4, -1);
+        ReorganizeArr ans = new ReorganizeArr();
+        System.out.println("Original: " + lst);
+        System.out.println("Answer: " + ans.rearrange(lst));
+    }
+}
