031a11f694a9ffd447c9d5d74e9eb3bdb972fdde
riya-rjha <riyaranjanjha2006@gmail.com>
2025-02-03 15:55:26 +0530

    Implement longest monotonic subarray calculation in LongestIncreasing.java

--- LeetCode Daily/LongestIncreasing.java
+++ LeetCode Daily/LongestIncreasing.java
@@ -1,9 +1,24 @@
-import java.util.Arrays;
-
 public class LongestIncreasing {
 
     public static int longestMonotonicSubarray(int[] nums) {
-        
+        int maxLen = 1;
+        int incLen = 1;
+        int decLen = 1;
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                incLen++;
+                decLen = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                decLen++;
+                incLen = 1;
+            } else {
+                incLen = 1;
+                decLen = 1;
+            }
+            maxLen = Math.max(maxLen, Math.max(incLen, decLen));
+        }
+        return maxLen;
     }
 
     public static void main(String[] args) {
